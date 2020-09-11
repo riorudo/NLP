bcb385af07353b53f666c750dab1476128886367
Michael Hochwieser <michael.hochwieser@gmail.com>
2020-09-11 09:13:52 +0200

    Fix issues after lighthouse audit.

--- src/client/js/formValidation.js
+++ src/client/js/formValidation.js
@@ -1,9 +1,9 @@
 function formValidation(formUrl, formText) {
     if (!formUrl && !formText) {
-        Client.renderError("Please enter an url or a text for entity extraction.");
+        Client.renderError("Please enter a url or a text for entity extraction.");
         return false;
     } else if (formUrl && formText) {
-        alert('If an url and a text is present, only the url will be considered.')
+        alert('If a url and a text is present, only the url will be considered.')
     }
     return true;
 }
